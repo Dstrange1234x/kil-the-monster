625ce634b6968a04dc0cd035a258eaf147925418
Dstrange1234x <74235077+Dstrange1234x@users.noreply.github.com>
2021-02-08 16:29:41 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,15 +1,16 @@
 const Engine = Matter.Engine;
 const World= Matter.World;
 const Bodies = Matter.Bodies;
-
+const Constraint= Matter.Constraint;
 
 var superhero
 var slingshot
 var engine,world;
+var b_image;
 
 function preload() {
 //preload the images here
-
+b_image=loadImage("images/GamingBackground.png");
 }
 
 function setup() {
@@ -18,12 +19,12 @@ function setup() {
   engine = Engine.create();
   world = engine.world;
 
-superhero=new Superhero(400,800,250);
-slingshot = new SlingShot(superhero.body,{x:400, y:800});
+superhero=new Superhero(400,500,250);
+slingshot = new SlingShot(superhero.body,{x:400, y:500});
 }
 
 function draw() {
-  background(0);
+  background(b_image);
   Engine.update(engine);
 
   superhero.display();
